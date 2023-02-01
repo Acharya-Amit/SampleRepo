23882a67127aaf38ca8bb9a54f3cde3725f7533a
Amit Acharya <amitacharya1454@gmail.com>
2023-02-01 05:54:44 +0530

    Base Task

---
+++ DrmoProject/src/mypackage/MyClass.java
@@ -0,0 +1,15 @@
+package mypackage;
+
+public class MyClass 
+{
+public static void main(String[] args)
+{
+
+	System.out.println("TestBase");
+	System.out.println("Pom.xml");
+	System.out.println("Utilities");
+	System.out.println("Resources");
+	System.out.println("Suite");
+}
+		
+}
